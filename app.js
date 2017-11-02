72c3c4ab0e043245831ffba24242cc3a8dc25f84
Romario Lawes <romario.lawes@mymona.uwi.edu>
2017-11-02 01:08:51 +0000

    Exercise 4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ window.onload = function() {
   
   function init() {
     let xhr = new XMLHttpRequest();
+    
     document.getElementById('search-btn').addEventListener("click", function() {
       xhr.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
@@ -10,6 +11,24 @@ window.onload = function() {
         }
       };
       xhr.open("GET", "request.php?q=" + document.getElementById('search-field').value, true);
+      xhr.send();
+    });
+    
+    document.getElementById('all-btn').addEventListener("click", function() {
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          let xmlOutput = "<ol>";
+          let xmlData = this.responseXML.getElementsByTagName('definition');
+          for (let i = 0; i < xmlData.length; i++) {
+            xmlOutput += "<li><h3>" + xmlData[i].getAttribute('name').toUpperCase() + "</h3>" + 
+                         "<p>" + xmlData[i].textContent + "</p>" + 
+                         "<p id='author'> - " + xmlData[i].getAttribute('author') + "</p></li>" + "<br>";
+          }
+          xmlOutput += "</ol>";
+          document.getElementById('result').innerHTML = xmlOutput;
+        }
+      };
+      xhr.open("GET", "request.php?q=all=true", true);
       xhr.send();
     });
   }
--- request.php
+++ request.php
@@ -4,15 +4,55 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if ($query === "all=true") {
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name="definition" author="John">
+            A statement of the exact meaning of a word, especially in a dictionary.
+        </definition>
+        
+        <definition name="bar" author="Mary">
+            A place that sells alcholic beverages.
+        </definition>
+        
+        <definition name="ajax" author="Kimberly">
+            Technique which involves the use of javascript and xml (or JSON).
+        </definition>
+        
+        <definition name="html" author="Paul">
+            The standard markup language for creating web pages and web applications.
+        </definition>
+        
+        <definition name="css" author="Romario">
+            A style sheet language used for describing the presentation of a document written in a markup language.
+        </definition>
+        
+        <definition name="javascript" author="Misheka">
+            A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+        </definition>
+        
+        <definition name="php" author="Alessia">
+            A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+        </definition>
+    </entries>';
+    
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+    
+} else {
+    $definition = [
+        "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+        "bar" => "A place that sells alcholic beverages.",
+        "ajax" => "Technique which involves the use of javascript and xml (or JSON).",
+        "html" => "The standard markup language for creating web pages and web applications.",
+        "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+        "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+        "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites.",
+    ];
+
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
+
